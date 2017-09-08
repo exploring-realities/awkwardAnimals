9216d94aa6ae7bcd28fa5c000bec610c708dc1db
Jonas <>
2017-09-08 04:05:45 +0200

    Add script stuff

---
+++ Unity Project/Hackathon_HololensVuforia/Origami/Assets/HackathonStateManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    Normal,
+    Accessibility
+}
+
+public class HackathonStateManager : MonoBehaviour
+{
+
+    public State currentState = State.Normal;
+    public bool arrowToBeChanged = false;
+    public static HackathonStateManager instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SwitchState()
+    {
+        if (currentState.Equals(State.Normal))
+        {
+            currentState = State.Accessibility;
+        }
+        else
+        {
+            currentState = State.Normal;
+        }
+        arrowToBeChanged = !arrowToBeChanged;
+    }
+}
